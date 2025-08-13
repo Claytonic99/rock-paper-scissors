9b2d18ec99ea1f0e6fefd7b33ed37e5d2cddec42
Clayton <clay10bg@gmail.com>
2025-08-13 12:32:29 -0600

    Add Multiple Rounds Functionality
    Added a game function that runs the round function five times and adds
    up the score each time. There is now a final message when the five games
    are over to tell the user if they won or not while also displaying
    the final score.

--- script.js
+++ script.js
@@ -26,65 +26,92 @@ function getComputerChoice() {
     // console.log(random);
     console.log(choice);
     return choice;
-}
+} //end function get computer choice
 
 function getHumanChoice() {
     let choice = prompt("rock, paper, or scissors?");
     console.log(choice);
     return choice;
-}
+} //end function get human choice
 
-let humanScore = 0;
-let computerScore = 0;
-let humanChoice = getHumanChoice();
-let computerChoice = getComputerChoice();
-let result = -1; //-1 is not determined, 0 is a tie, 1 is user wins, 2 is computer wins
+function playRound(humanChoice, computerChoice) {
+    // Remove case sensitivity
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+    let result = -1; //-1 is not determined, 0 is a tie, 1 is user wins, 2 is computer wins
 
-switch (humanChoice)
-{
-    case "rock":
-    switch (computerChoice)
+    // Determine Results
+    switch (humanChoice)
     {
-        case "rock": result = 0; break; //a tie
-        case "paper": result = 2; break; //computer wins
-        case "scissors": result = 1; break; //human wins
-    }
-    break;
+        case "rock":
+        switch (computerChoice)
+        {
+            case "rock": result = 0; break; //a tie
+            case "paper": result = 2; break; //computer wins
+            case "scissors": result = 1; break; //human wins
+        }
+        break;
 
-    case "paper":
-    switch (computerChoice)
+        case "paper":
+        switch (computerChoice)
+        {
+            case "rock": result = 1; break; //human wins
+            case "paper": result = 0; break; //a tie
+            case "scissors": result = 2; break; //computer wins
+        }
+        break;
+
+        case "scissors":
+        switch (computerChoice)
+        {
+            case "rock": result = 2; break; //computer wins
+            case "paper": result = 1; break; //human wins
+            case "scissors": result = 0; break; //a tie
+        }
+        break;
+    }
+
+    return result;
+} //end function play round
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    let humanChoice = "";
+    let computerChoice = "";
+    let result = -1;
+
+    for (let round = 0; round < 5; round++)
     {
-        case "rock": result = 1; break; //human wins
-        case "paper": result = 0; break; //a tie
-        case "scissors": result = 2; break; //computer wins
+        console.log(humanScore+":"+computerScore);
+        humanChoice = getHumanChoice();
+        computerChoice = getComputerChoice();
+        result = playRound(humanChoice, computerChoice); //get result
+        
+        // Show the result
+        switch (result)
+        {
+            case 0:
+            console.log("It's a tie! Both picked "+humanChoice);
+            break;
+
+            case 1:
+            console.log("You win! "+humanChoice+" beats "+computerChoice);
+            humanScore++;
+            break;
+
+            case 2:
+            console.log("You lose! "+computerChoice+" beats "+humanChoice);
+            computerScore++;
+            break;
+        } //end switch result
     }
-    break;
 
-    case "scissors":
-    switch (computerChoice)
-    {
-        case "rock": result = 2; break; //computer wins
-        case "paper": result = 1; break; //human wins
-        case "scissors": result = 0; break; //a tie
-    }
-    break;
-}
+    // End of game
+    console.log("Final Score "+humanScore+":"+computerScore);
+    if (humanScore > computerScore) console.log("Congratulations! You're the winner!\nF5 to play again?");
+    else if (humanScore < computerScore) console.log("So sorry, you lose.\nF5 to play again?");
+    else console.log("Wow! A tie game!\nF5 to play again?");
+} //end function play game
 
-switch (result)
-{
-    case 0:
-    console.log("It's a tie.");
-    break;
-
-    case 1:
-    console.log("Human wins!");
-    humanScore++;
-    break;
-
-    case 2:
-    console.log("Computer wins!");
-    computerScore++;
-    break;
-}
-
-console.log(humanScore+":"+computerScore);
+playGame();
