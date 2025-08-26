31f4abbbd5d70b60bdb228e2af1461d6e1321790
Clayton <clay10bg@gmail.com>
2025-08-26 15:50:24 -0600

    Create UI Elements for RPS Game
    Created three buttons for rock, paper, and scissors for the user to
    press to make their selection. The game logic has also been changed
    so that it makes a check with what the computer randomly chose only
    when the user presses one of the buttons. When either the user or the
    computer gets to 5 points, the game ends and the buttons are removed.
    The running score is displayed at all times.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// const { createElement } = require("react");
+
 /*Pseudocode
 User enters rock, paper, or scissors and the answer is stored in a variable
 CPU randomly chooses rock, paper, or scissors and is stored in a variable
@@ -28,16 +30,7 @@ function getComputerChoice() {
     return choice;
 } //end function get computer choice
 
-function getHumanChoice() {
-    let choice = prompt("rock, paper, or scissors?");
-    console.log(choice);
-    return choice;
-} //end function get human choice
-
 function playRound(humanChoice, computerChoice) {
-    // Remove case sensitivity
-    humanChoice = humanChoice.toLowerCase();
-    computerChoice = computerChoice.toLowerCase();
     let result = -1; //-1 is not determined, 0 is a tie, 1 is user wins, 2 is computer wins
 
     // Determine Results
@@ -59,6 +52,9 @@ function playRound(humanChoice, computerChoice) {
     return result;
 } //end function play round
 
+
+
+
 function playGame() {
     let humanScore = 0;
     let computerScore = 0;
@@ -66,10 +62,11 @@ function playGame() {
     let computerChoice = "";
     let result = -1;
 
-    for (let round = 0; round < 5; round++)
-    {
-        console.log(humanScore+":"+computerScore);
-        humanChoice = getHumanChoice();
+    let btn = document.querySelector("#menu");
+    btn.addEventListener("click", (event) => {
+        let target = event.target;
+        console.log("Button pressed: "+target.id);
+        humanChoice = target.id;
         computerChoice = getComputerChoice();
         result = playRound(humanChoice, computerChoice); //get result
         // Show the result
@@ -77,25 +74,52 @@ function playGame() {
         {
             case 0:
             console.log("It's a tie! Both picked "+humanChoice);
+            const tie = document.querySelector("#results");
+            tie.textContent = "It's a tie! Both picked "+humanChoice;
             break;
 
             case 1:
             console.log("You win! "+humanChoice+" beats "+computerChoice);
+            const win = document.querySelector("#results");
+            win.textContent = "You win! "+humanChoice+" beats "+computerChoice;
             humanScore++;
             break;
 
             case 2:
             console.log("You lose! "+computerChoice+" beats "+humanChoice);
+            const lose = document.querySelector("#results");
+            lose.textContent = "You lose! "+computerChoice+" beats "+humanChoice;
             computerScore++;
             break;
         } //end switch result
-    }
 
-    // Show final score
-    console.log(`Final Score ${humanScore}:${computerScore}`);
-    if (humanScore > computerScore) console.log("Congratulations! You're the winner!\nF5 to play again?");
-    else if (humanScore < computerScore) console.log("So sorry, you lose.\nF5 to play again?");
-    else console.log("Wow! A tie game!\nF5 to play again?");
+        const score = document.querySelector("#score");
+        if (humanScore == 5 || computerScore == 5)
+        {
+            // Remove Buttons
+            btn.remove();
+            // Show final score
+            console.log(`Final Score ${humanScore}:${computerScore}`);
+            if (humanScore > computerScore)
+            {
+                score.textContent = `The final score is ${humanScore} to ${computerScore}.
+                Congratulations! You're the winner! F5 to play again?`;
+                console.log("Congratulations! You're the winner!\nF5 to play again?");
+            }
+            else if (humanScore < computerScore)
+            {
+                score.textContent = `The final score is ${humanScore} to ${computerScore}.
+                So sorry, you lose. F5 to play again?`;
+                console.log("So sorry, you lose.\nF5 to play again?");
+            }
+        }
+        else
+        {
+            score.textContent = `The score is now ${humanScore} to ${computerScore}`;
+        }
+    });
+
+    
 } //end function play game
 
 playGame();
